b63c0536d8a831fdd10997077acf5042a3acea14
AA1-34-Ganesh <guntaganesh1@gmail.com>
2026-05-07 21:43:25 +0530

    Find how many pairs present in an array equal to target sum.

---
+++ src/com/placement/questions/PairSum.java
@@ -0,0 +1,16 @@
+package com.placement.questions;
+
+public class PairSum {
+    public static void main(String[] args) {
+        int[] arr = {7, 5, 3, 2, 10, 6};
+        int target = 8;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(arr[i] + " + " + arr[j] + " = " + target);
+                }
+            }
+        }
+    }
+}
